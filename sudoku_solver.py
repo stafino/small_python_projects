79046c5d221d00cef8c7f6e6e4002cf720a24fbd
stafino <oliverstaf1@gmail.com>
2022-11-05 19:31:18 +0000

    sudoku_solver.py - print_board function

---
+++ sudoku_solver.py
@@ -0,0 +1,31 @@
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+# function to print the board with the spaces so it's easy to look at and understand for a human
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - - - -")
+
+        for j in range(len(board[0])):
+            # print a | behind every third element but not at the beginning
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            # when we access the last element of the row, print [i][j]
+            if j == 8:
+                print(board[i][j])
+            # otherwise
+            else:
+                print(str(board[i][j]) + " ", end=" ")
+
+print_board(board)
