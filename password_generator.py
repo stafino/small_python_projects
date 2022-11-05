facd01301f7ded91eec706d5a2a121a8ced28beb
stafino <oliverstaf1@gmail.com>
2022-11-05 17:18:05 +0000

    finished basic password generator

---
+++ password_generator.py
@@ -0,0 +1,17 @@
+# Plan
+import random
+print("Welcome to the password generator")
+
+# 1. Create an alphabet
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789¬£$%^&*()_+=-[]{}#;:./,|@~!"
+password = ""
+
+# 2. Take input from user how long he wants the password to be
+print("Please input how long would you like your password to be.")
+user_input = int(input())
+
+# 3. Generate a random password on the given alphabet with the length provided
+for index in range(user_input):
+    password += random.choice(alphabet)
+# 4. Log the password to the console with a message
+print("This is your automatically generated, secure password: ", password)
