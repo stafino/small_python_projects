c458ff738725ec4c57ca5635652bce7edf888baf
stafino <oliverstaf1@gmail.com>
2022-11-11 17:15:16 +0000

    CSE_Bad_example.py - fixed the CSE mistake

--- CSE_longer_example.py
+++ CSE_longer_example.py
@@ -14,7 +14,7 @@ pingpong_weight = 0.003  # weight of pingpong ball in grams
 volume_of_pingpong = 4 / 3 * 3.14 * pingpong_radius * pingpong_radius # 1 ping pong ball volume - Volume= 4/3*(pi)(r cubed).
 
 # Transportation
-truck_capacity = 5_000  # what's the maximum material one car can transport - in metres^3
+truck_capacity = 5_000  # what's the maximum material one car can transport - in kg
 
 
 class CodeTimer:
@@ -35,19 +35,19 @@ with CodeTimer('No CSE used - 20_000 cases'):
     for i in range(20_000):
         house_volume = house_length * house_width * house_height  # calculate the volume of the given cuboid
         number_of_pingpongs_in_house = house_volume / volume_of_pingpong  # how many ping pong balls fit in the house
-        total_weight = number_of_pingpongs_in_house * pingpong_weight
-        trucks_needed = total_weight / truck_capacity
-
+        total_weight_pingpong = number_of_pingpongs_in_house * pingpong_weight
+        trucks_needed = total_weight_pingpong / truck_capacity
         neighbourhood_area = house_length * house_width * number_of_houses_in_neighbourhood  # for some different project we just want to see what's the foundations area of all houses in the given neighbourhood
-    print(f'Total number of all balls: {number_of_pingpongs_in_house}. Total weight of all balls: {total_weight}. Total cars needed: {trucks_needed}')
+
+    print(f'Total number of all balls: {number_of_pingpongs_in_house}. Total weight of all balls: {total_weight_pingpong}. Total cars needed: {trucks_needed}')
 
 with CodeTimer('CSE used - 20_000 cases'):
     for i in range(20_000):
         temporary_value = house_length * house_width  # base of the cuboid
-        house_volume = house_length * house_width * house_height  # calculate the volume of the given cuboid
+        house_volume = temporary_value * house_height  # calculate the volume of the given cuboid
         number_of_pingpongs_in_house = house_volume / volume_of_pingpong  # how many ping pong balls fit in the house
-        total_weight = number_of_pingpongs_in_house * pingpong_weight
-        trucks_needed = total_weight / truck_capacity
+        total_weight_pingpong = number_of_pingpongs_in_house * pingpong_weight
+        trucks_needed = total_weight_pingpong / truck_capacity
 
         neighbourhood_area = temporary_value * number_of_houses_in_neighbourhood  # for some different project we just want to see what's the foundations area of all houses in the given neighbourhood
 
@@ -59,8 +59,8 @@ with CodeTimer('No CSE used - 20 million cases'):
     for i in range(20_000_000):
         house_volume = house_length * house_width * house_height  # calculate the volume of the given cuboid
         number_of_pingpongs_in_house = house_volume / volume_of_pingpong  # how many ping pong balls fit in the house
-        total_weight = number_of_pingpongs_in_house * pingpong_weight
-        trucks_needed = total_weight / truck_capacity
+        total_weight_pingpong = number_of_pingpongs_in_house * pingpong_weight
+        trucks_needed = total_weight_pingpong / truck_capacity
 
         neighbourhood_area = house_length * house_width * number_of_houses_in_neighbourhood  # for some different project we just want to see what's the foundations area of all houses in the given neighbourhood
 
@@ -68,9 +68,9 @@ with CodeTimer('No CSE used - 20 million cases'):
 with CodeTimer('CSE used - 20 million cases'):
     for i in range(20_000_000):
         temporary_value = house_length * house_width  # base of the cuboid
-        house_volume = house_length * house_width * house_height  # calculate the volume of the given cuboid
+        house_volume = temporary_value * house_height  # calculate the volume of the given cuboid
         number_of_pingpongs_in_house = house_volume / volume_of_pingpong  # how many ping pong balls fit in the house
-        total_weight = number_of_pingpongs_in_house * pingpong_weight
-        trucks_needed = total_weight / truck_capacity
+        total_weight_pingpong = number_of_pingpongs_in_house * pingpong_weight
+        trucks_needed = total_weight_pingpong / truck_capacity
 
         neighbourhood_area = temporary_value * number_of_houses_in_neighbourhood  # for some different project we just want to see what's the foundations area of all houses in the given neighbourhood
